55420ed88cd216fdb18efb530f2d271ecb94e9bb
SushsaviG <86051136+SushsaviG@users.noreply.github.com>
2021-06-24 20:27:48 +0530

    Add files via upload

---
+++ demo.7/Health.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demo7
+{
+    class Health
+    {
+        public double height;
+        public double weight;
+        public double bmi;
+        public Health(double height,double weight)
+
+        {
+            this.height = height;
+            this.weight = weight;
+
+        }
+    }
+}
