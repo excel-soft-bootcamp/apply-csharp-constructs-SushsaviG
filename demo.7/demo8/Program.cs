2e7acf1da2b5a2952712b78bfcc9df612fc2f613
SushsaviG <86051136+SushsaviG@users.noreply.github.com>
2021-06-24 20:32:36 +0530

    Add files via upload

---
+++ demo.7/demo8/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demo7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            double bmi = 0;
+
+            Health h = new Health(1.372,56);
+           double  r1=h.M1(1.372, 56);
+            Console.WriteLine(r1);
+            Console.ReadLine();
+            
+
+        }
+    }
+}
