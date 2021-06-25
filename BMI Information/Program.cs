d17c7680d649b37c6a152f374b1d197678e3373c
SushsaviG <86051136+SushsaviG@users.noreply.github.com>
2021-06-25 12:53:44 +0530

    Add files via upload

---
+++ BMI Information/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demo7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            double bmi = 0;
+
+            BMIDATA h = new BMIDATA(1.372,56);
+            h.Bmicalculationandvalidation();
+
+        }
+    }
+}
