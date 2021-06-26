70db853f407f9594cb4b90744f7997d08e07051b
SushsaviG <86051136+SushsaviG@users.noreply.github.com>
2021-06-26 14:31:45 +0530

    Add files via upload

---
+++ College Identity Information Application/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace College_Identity_Information_Application
+{
+    class Program
+     
+        static void Main(string[] args)
+        {
+              StudentId SI = new StudentId("Swathi", "science", "141", "23-03-1997", "2021-2024");
+                 Console.WriteLine(s1);                  
+        }
+    }
+}
