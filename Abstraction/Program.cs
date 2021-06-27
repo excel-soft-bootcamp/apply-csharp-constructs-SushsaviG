ca44bbf4a019707a114f676f52f2af6cdc0156fd
SushsaviG <86051136+SushsaviG@users.noreply.github.com>
2021-06-27 21:05:19 +0530

    Add files via upload

---
+++ Abstraction/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            VeriCorEngine _nexon = new VeriCorEngine();
+            TATACAR car = new TATACAR(_nexon);
+            car.Drive();
+            DiCorEngine _harrier = new DiCorEngine();
+            TATACAR car1 = new TATACAR(_nexon);
+            car1.Drive();
+            MultiJetEngine _hexa = new MultiJetEngine();
+            TATACAR car3 = new TATACAR(_hexa);
+            car3.Drive();
+
+        }
+    }
+}
---
+++ Abstraction/TATACAR.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstraction
+{
+    public class TATACAR
+    {
+        Iengine _engine =null;
+        public TATACAR(Iengine engine)
+        {
+            this._engine = engine;
+        }
+
+
+        public void Drive()
+        {
+
+            _engine.start();
+        }
+        public void Halt()
+        {
+
+            _engine.stop();
+        }
+    }
+    public interface Iengine
+    {
+        void start();
+        void stop();
+    }
+    public class DiCorEngine : Iengine
+    {
+
+        public void start() { }
+        public void Stop() { }
+    }
+
+    public class VeriCorEngine : Iengine
+    {
+        public void start() { }
+        public void Stop() { }
+
+    }
+
+    public class MultiJetEngine : Iengine
+    {
+        public void start() { }
+        public void Stop() { }
+    }
+
+
+}
