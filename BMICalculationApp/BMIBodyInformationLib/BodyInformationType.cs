293784b2692db820d851d446753f6693e42f80b6
Shubhamm22 <86050595+Shubhamm22@users.noreply.github.com>
2021-07-06 19:19:11 +0530

    Add files via upload

---
+++ BMICalculationApp/BMIBodyInformationLib/BodyInformationType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace BMIBodyInformationLib
+{
+    public class BodyInformationType
+    {
+        public double ReadBodyInformation(string message)
+        {
+            Console.WriteLine(message);
+                double value = Convert.ToDouble(Console.ReadLine());
+            return value;
+        }
+    }
+}
