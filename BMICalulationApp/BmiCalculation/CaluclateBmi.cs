8751eae8b7514e730b4c3bb958e2a74c60d44a57
SushsaviG <86051136+SushsaviG@users.noreply.github.com>
2021-07-06 18:00:42 +0530

    Add files via upload

---
+++ BMICalulationApp/BmiCalculation/CaluclateBmi.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BmiCalculation
+{
+    public class CaluclateBmi
+    {
+        public static int result;
+
+        public static int CalculateBMIResult(int height, int weight)
+        {
+            return result = weight / height;
+
+        }
+    }
+}
