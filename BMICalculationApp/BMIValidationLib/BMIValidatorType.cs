06240a86d926bfbbc5a8fa4d198cb1a493d1868a
Shubhamm22 <86050595+Shubhamm22@users.noreply.github.com>
2021-07-06 19:22:04 +0530

    Add files via upload

---
+++ BMICalculationApp/BMIValidationLib/BMIValidatorType.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace BMIValidationLib
+{
+    public class BMIValidatorType
+    {
+        
+            public string BMIValidate(double bmiResult)
+            {
+                if (bmiResult < 18.5)
+                {
+                    return "Overweight";
+                }
+                else if (bmiResult >= 25)
+                {
+                    return "Healthy";
+                }
+                else
+                {
+                    return "Invalid Input";
+                }
+            }
+        
+    }
+}
