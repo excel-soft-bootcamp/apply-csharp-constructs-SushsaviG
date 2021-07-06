9e1c9f0c80b300d151cb6106adef7b6ccfc402ac
Shubhamm22 <86050595+Shubhamm22@users.noreply.github.com>
2021-07-06 19:49:11 +0530

    Add files via upload

---
+++ BMICalculationApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace BMICalculationApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BMIBodyInformationLib.BodyInformationType _bodyinformation = new BMIBodyInformationLib.BodyInformationType();
+            double _heightValue = _bodyinformation.ReadBodyInformation("enter your height");
+            double _weightValue = _bodyinformation.ReadBodyInformation("enter your weight");
+
+            BMICalculatorLib.BMICalculatorType _bmicalculate = new BMICalculatorLib.BMICalculatorType();
+            double _bmivalue = _bmicalculate.CalculateBMIResult(_heightValue, _weightValue);
+
+            BMIValidationLib.BMIValidatorType _bmivalidate = new BMIValidationLib.BMIValidatorType();
+            string _result = _bmivalidate.BMIValidate(_bmivalue);
+
+            BMIConsoleDisplayLib.DisplayType _display = new BMIConsoleDisplayLib.DisplayType();
+            _display.BMIDisplay(_result);
+
+
+        }
+    }
+}
