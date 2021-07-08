dc7712bc201540d969b757cfd6ae175f8b78e3ab
SushsaviG <86051136+SushsaviG@users.noreply.github.com>
2021-07-08 16:50:42 +0530

    Add files via upload

---
+++ BMICalculationApplication/BMIbodyInformationLib/BMIBodyInformationType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BMIbodyInformationLib
+{
+    public class BMIBodyInformationType
+    {
+        public double ReadBodyInformation(string message)
+        {
+           
+            double value = Convert.ToDouble(Console.ReadLine());
+            return value;
+        }
+    }
+}
